90551971a71e5f41694170fa0e1723aee9fbb638
MP-18 <miguel.papa101328@my.lsgh.edu.ph>
2021-05-03 13:24:01 +0800

    4 files modified, 1 added

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -3,12 +3,29 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
-  templateUrl: 'contact.html'
+  templateUrl: 'contact.html',
+  styleUrls: ['contact.css']
 })
 export class ContactPage {
+  w: number;
+  a: number;
+  d: number;
+  METS: number;
 
-  constructor(public navCtrl: NavController) {
+  C: number;
+  BC: number;
+  WL: number;
 
+  constructor() {}
+
+  calculateCBC() {
+    this.C = this.METS * 3.5 * (this.w / 200);
+    this.C = parseFloat(this.C.toFixed(2));
+
+    this.BC = this.C * this.d;
+    this.BC = parseFloat(this.BC.toFixed(2));
+
+    this.WL = this.C / 7700;
+    this.WL = parseFloat(this.WL.toFixed(2));
   }
-
 }
